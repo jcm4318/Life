60b350e7fde08655e2e55246c418f28c8d261941
jcm4318 <73109613+jcm4318@users.noreply.github.com>
2020-12-17 19:24:39 +0000

    Update Life.f
    Final version of the core life code

--- Life.f
+++ Life.f
@@ -1,23 +1,35 @@
-{ ---------------------------- Welcome to Life.f, Comic Sans edition ---------------------------- }
+{ -------------------------------------- Welcome to Life ---------------------------------------- }
 
+{ ----------------------------------- Here are some constants ----------------------------------- }
 
-
-
-
-
-
-{ ------------------------------------ Here are the exercises: ------------------------------------ }
-
-4 constant num-cols                                                            { the side length of the array }
+500 constant num-cols \ array side length
 num-cols num-cols * constant num-vals 
 
-create life-array num-vals allocate drop 
-life-array num-vals 0 fill .
-cr cr ." Life-array created "
+variable delay-time
+variable survives
+variable survives2
+variable survives3
+variable born
+variable born2
+variable born3
 
-create numalive-array num-vals allocate drop 
-numalive-array num-vals 0 fill .
-cr cr ." Numalive-array created " cr cr
+\ game of life rules:
+3 born !
+2 survives !
+99 born2 !
+99 born3 !
+99 survives2 !
+99 survives3 !
+100 delay-time !
+
+create life-array num-vals allot \ create the life array
+cr cr ." Life-array created " TYPE
+
+create seed num-vals allot \ create the seed array (for saving/loading random inits)
+
+create numalive-array num-vals allot \ create the numalive array
+\ numalive-array num-vals 0 fill .
+cr cr ." Numalive-array created " TYPE cr cr
 
 { ---------------------------------- Here are the dependencies: ---------------------------------- }
 
@@ -25,6 +37,7 @@ REQUIRES C:\Users\james\Documents\Physics\Y3D3LabTesting\RND               { Loa
 REQUIRES C:\Users\james\Documents\Physics\Y3D3LabTesting\Graphics_V6_Single_Scaled_BMP_Window 
 REQUIRES C:\Users\james\Documents\Physics\Y3D3LabTesting\Test_File_IO_Tools 
 REQUIRES C:\Users\james\Documents\Physics\Y3D3LabTesting\Text_Output_Window_V1
+\ REQUIRES C:\Users\james\Documents\Physics\Y3D3LabTesting\Life-Shapes
 
 { ---------------------------------------- No longer useful ----------------------------------------- }
 
@@ -45,7 +58,25 @@ loop
 : empty-stack \ print the contents of the stack and reset it
 0 begin . depth 0 <= until ;
 
-{ ------------------------------ Array Accessing and Manipulating ------------------------------- }
+{ --------------------------------------- File I/O Commands --------------------------------------- }
+
+: write-file-data \ writes the number of alive/dead cells to a file
+total-alive @ (.)         test-file-id @ write-file drop
+s" ,"         test-file-id @ write-file drop
+total-dead @ (.) test-file-id @ write-line drop ;
+
+{ --------------------------------------- Graphics Commands --------------------------------------- }
+
+: update-sim \ updates the current bitmap window to the position of the game
+bmp-address @ Calc-bmp-White
+bmp-address @ bmp-to-screen-stretch ;
+
+: show-sim \ displays the current life-array as a bitmap
+go-stretch-mine 
+new-file 
+write-file-data ;
+
+{ ------------------------------ Array Accessing and Manipulating --------------------------------- }
 
 : life-array@ \ access a value in the life-array using x,y coordinates
 0 pick num-cols <
@@ -54,10 +85,18 @@ loop
 4 pick 0 >= 
 and and and if
 num-cols * + life-array + C@ 
-else . . 0 
+else drop drop 0 
 then ;
 
-: life-array! num-cols * + life-array swap + C! ; \ change a value in the life-array using x,y coordinates
+: life-array! \ change a value in the life-array using x,y coordinates: num x y life-array!
+0 pick num-cols <
+1 pick 0 >=
+3 pick num-cols <
+4 pick 0 >= 
+and and and if
+num-cols * + life-array swap + C! 
+else drop drop drop
+then ; 
 
 : numalive-array@ num-cols * + numalive-array + C@ ; \ access a value in the numalive-array using x,y, coordinates
 
@@ -77,108 +116,15 @@ I J numalive-array@ 4 .R
 loop cr cr
 -1 +loop  ;
 
-: tr 1 rot rot life-array! ; \ sets a cell in life-array to be alive given x,y coordinates
+: tr 1 rot rot life-array! update-sim ; \ sets a cell in life-array to be alive given x,y coordinates
 
-: fa 0 rot rot life-array! ; \ set a cell in life-array to be dead given x,y coordinates
+: fa 0 rot rot life-array! update-sim ; \ set a cell in life-array to be dead given x,y coordinates
 
 : dead-linear \ set a cell in life-array to be dead given the 'linear' cell number
 0 life-array rot + C! ;
 
 : alive-linear \ set a cell in life-array to be alive given the 'linear' cell number
 1 life-array rot + C! ;
-
-{ ----------------------------------- Gameplay Inner workings ------------------------------------ }
-
-: numalive-next \ use the numalive-array to change the life-array to the next set of cells
-num-vals 0 do
-        I I numalive-array + C@
-        case
-                0 of dead-linear endof
-                1 of dead-linear endof
-                2 of I life-array + C@ 1 = 
-                        if alive-linear
-                        else dead-linear
-                        then
-                endof
-                3 of alive-linear endof
-                4 of dead-linear endof
-                5 of dead-linear endof
-                6 of dead-linear endof
-                7 of dead-linear endof
-                8 of dead-linear endof
-        endcase
-loop 
-show-array ;
-
-: life-next \ counts the number of alive cells around each cell in life-array and sends to numalive-array
-0 LOCALS| numalive |
-show-array
-num-cols 0 do \ for each row
-num-cols 0 do \ for each column
-show-array
-cr cr
-." I,J: " I . J .
-cr
-
-0 TO numalive \ reset the number of alive cells
-
-cr
-." Bottom left " 
-I 1 - J 1 - life-array@ dup . 1 = \ bottom left
-if numalive 1 + TO numalive then
-\ numalive .
-
-cr
-." Bottom middle " 
-I J 1 - life-array@ dup . 1 = \ bottom middle
-if numalive 1 + TO numalive then
-\ numalive .
-
-cr
-." Bottom right " 
-I 1 + J 1 - life-array@ dup . 1 = \ bottom middle
-if numalive 1 + TO numalive then
-\ numalive .
-
-cr
-." Middle left " 
-I 1 - J life-array@ dup . 1 = \ middle left
-if numalive 1 + TO numalive then
-\ numalive . 
-
-cr
-." Middle right " 
-I 1 + J life-array@ dup . 1 = \ middle right
-if numalive 1 + TO numalive then
-\ numalive .
-
-cr
-." Top left " 
-I 1 - J 1 + life-array@ dup . 1 = \ top left
-if numalive 1 + TO numalive then
-\ numalive .
-
-cr
-." Top middle " 
-I J 1 + life-array@ dup . 1 = \ top middle
-if numalive 1 + TO numalive then
-\ numalive .
-
-cr
-." Top right " 
-I 1 + J 1 + life-array@ dup . 1 = \ top right
-if numalive 1 + TO numalive then
-\ numalive .
-
-cr ." Numalive: "
-numalive .
-
-numalive I J numalive-array!
-
-loop 
-loop ;
-
-{ ------------------------------------------- Statistics ------------------------------------------- }
 
 : count-cells
 0 locals| alive |
@@ -187,97 +133,133 @@ I life-array + C@ 1 =
 if alive 1 + TO alive
 then
 loop 
-cr cr
-." Number alive: " alive . cr
-." Number dead: " num-vals alive - . cr cr 
 alive total-alive ! 
 num-vals alive - total-dead ! ;
-
-{ ------------------------------------ Array Initial Conditions ------------------------------------ }
 
 : rst-array \ reset all values in the life-array to 0
 life-array num-vals 0 fill 
 count-cells
-show-array ;
+update-sim ;
 
-: linear-array \ set values to in life-array to a linear sequence
-num-vals 0 do 
-I life-array I + C! loop 
-count-cells
-show-array ;
+{ ----------------------------------- Gameplay Inner workings ------------------------------------ }
 
-: 8rnd-array \ set each value in life-array to a random number between 0 and 8 inclusive
-num-vals 0 
-do 9 RND life-array I  + C! 
+: life-next \ counts the number of alive cells around each cell in life-array and sends to numalive-array
+0 LOCALS| numalive |
+num-cols 0 do \ for each row
+num-cols 0 do \ for each column
+
+0 TO numalive \ reset the number of alive cells
+
+\ check if each surrounding cell is alive
+I 1 - J 1 - life-array@  1 = \ bottom left
+if numalive 1 + TO numalive then
+
+I J 1 - life-array@  1 = \ bottom middle
+if numalive 1 + TO numalive then
+
+I 1 + J 1 - life-array@  1 = \ bottom middle
+if numalive 1 + TO numalive then
+
+I 1 - J life-array@  1 = \ middle left
+if numalive 1 + TO numalive then
+ 
+I 1 + J life-array@  1 = \ middle right
+if numalive 1 + TO numalive then
+
+I 1 - J 1 + life-array@  1 = \ top left
+if numalive 1 + TO numalive then
+
+I J 1 + life-array@  1 = \ top middle
+if numalive 1 + TO numalive then
+
+I 1 + J 1 + life-array@ 1 = \ top right
+if numalive 1 + TO numalive then
+
+numalive I J numalive-array!
+
 loop 
-show-array ;
+loop ;
 
-: iden-array \ set life-array to the identity matrix (Not working properly since show-array changed)
-life-array num-vals 0 fill 
-cr cr
-num-cols 0 do
-1 I I life-array! 
-loop
-count-cells
-show-array ;
+: numalive-next \ use the numalive-array to change the life-array to the next set of cells
+num-vals 0 do
+        I I numalive-array + C@
+        case
+                survives @ of I life-array + C@ 1 = 
+                        if alive-linear
+                        else dead-linear
+                        then
+                endof
+                survives2 @ of I life-array + C@ 1 = 
+                        if alive-linear
+                        else dead-linear
+                        then
+                endof
+                survives3 @ of I life-array + C@ 1 = 
+                        if alive-linear
+                        else dead-linear
+                        then
+                endof
+                born @ of alive-linear endof
+                born2 @ of alive-linear endof
+                born3 @ of alive-linear endof
+                dup of dead-linear endof
+        endcase
+loop ;
 
-: ones-array
-life-array num-vals 1 fill 
-count-cells
-show-array ;
+{ ------------------------------------------- Statistics ------------------------------------------- }
 
-: glider-array \ initialise life-array with a glider in the top left corner
-life-array num-vals 0 fill 
-2 num-cols 1 - tr
-2 num-cols 2 - tr
-2 num-cols 3 - tr
-1 num-cols 3 - tr
-0 num-cols 2 - tr
-count-cells
-show-array ;
+: save-seed \ saves the current state of life-array to seed
+num-vals 0 do
+i life-array + c@ i seed + c!
+loop ;
 
-: pipent-array \ initialises life-array with a pi-pentomino methuselah
-\ assumes the array has even side length
-life-array num-vals 0 fill 
-num-cols 1 - 2 / num-cols 1 - 2 / 1 + tr
-num-cols 1 - 2 / 1 - num-cols 1 - 2 / 1 + tr
-num-cols 1 - 2 / 1 + num-cols 1 - 2 / 1 + tr
-num-cols 1 - 2 / 1 - num-cols 1 - 2 / tr
-num-cols 1 - 2 / 1 - num-cols 1 - 2 / 1 - tr
-num-cols 1 - 2 / 1 + num-cols 1 - 2 / tr
-num-cols 1 - 2 / 1 + num-cols 1 - 2 / 1 - tr
+: load-seed \ loads the seed into life-array 
+num-vals 0 do
+i seed + c@ i life-array + c!
+loop 
 count-cells
-show-array ;
+update-sim ;
 
 { -------------------------------------------- Gameplay -------------------------------------------- }
 
-: nt life-next numalive-next count-cells show-array ; \ shorthand for 'next turn', runs the whole iteration
-
-: update-sim \ updates the current bitmap window to the position of the game
-bmp-address @ Calc-bmp-White
-bmp-address @ bmp-to-screen-stretch ;
-
-: show-sim \ displays the current life-array as a bitmap
-go-stretch-mine ;
+: nt life-next numalive-next count-cells write-file-data ; \ shorthand for 'next turn'; runs the whole iteration
 
 : show-nt \ manually runs nt and update-sim
 life-next
 numalive-next
-show-array
 bmp-address @ Calc-bmp-White
 bmp-address @ bmp-to-screen-stretch
-count-cells ;
+count-cells 
+write-file-data ;
 
-: run-sim \ runs the simulation as a bitmap
+: run-sim \ opens a new bitmap and runs the simulation
 go-stretch-mine
 count-cells
-2000 ms
+new-file
+write-file-data
+save-seed
+1000 ms
 begin
 nt
+write-file-data
 bmp-address @ Calc-bmp-White        { Add random pixels to .bmp in memory             }
 bmp-address @ bmp-to-screen-stretch { Stretch .bmp to display window                  }
+1 ms
 key?
-count-cells
-\ 1 * total-alive !
-1000 ms
 until ;
+
+: play-sim \ plays the currently opened sim
+save-seed
+begin
+nt
+write-file-data
+bmp-address @ Calc-bmp-White        { Add random pixels to .bmp in memory             }
+bmp-address @ bmp-to-screen-stretch { Stretch .bmp to display window                  }
+delay-time @ ms
+key?
+until ;
+
+{ ---------------------------------------------- Init ---------------------------------------------- }
+
+show-sim \ open a sim on init
+rst-array \ reset to empty space
